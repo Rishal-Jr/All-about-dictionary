63204567e2d47831fbbb56120982785a8787d9e1
Rishal-Jr <rmrazni@gmail.com>
2025-02-26 12:05:35 +0530

    country code

---
+++ country code checker.py
@@ -0,0 +1,5 @@
+country_code={'Sri-Lanka':'0094','India':'0091','Australia':'0025'}
+print("Country code for Sri-Lanka :")
+print(country_code.get('Sri-Lanka','Not found'))
+print("Country code for Japan :")
+print(country_code.get('Japan','Not found'))
